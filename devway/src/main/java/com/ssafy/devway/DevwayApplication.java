c04f7da4ba15cf9e1672bad237ac034316cd763b
김준섭 <wns1915@naver.com>
2024-04-22 22:01:55 +0900

    feat : init spring boot pjt

---
+++ devway/src/main/java/com/ssafy/devway/DevwayApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.devway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevwayApplication.class, args);
+	}
+
+}
---
+++ devway/src/main/java/com/ssafy/devway/domain/test/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.ssafy.devway.domain.test.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class TestController {
+    @GetMapping
+    public String home() {
+        return "Hello Devway";
+    }
+}
