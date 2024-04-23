493454660af02101beef2ffce7e544c1cdb7e883
김준섭 <wns1915@naver.com>
2024-04-23 12:20:14 +0900

    fix : fix dockerfile

--- devway/src/main/java/com/ssafy/devway/domain/test/controller/TestController.java
+++ devway/src/main/java/com/ssafy/devway/domain/test/controller/TestController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String home() {
-        return "Hello Oringe";
+        return "성소수준동아";
     }
 }
