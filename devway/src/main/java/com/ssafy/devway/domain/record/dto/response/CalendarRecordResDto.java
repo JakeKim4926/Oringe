ff1e8fd3326ea88c2ad1dc098ddfe86860afffe3
양성주 <ztjdwnz@naver.com>
2024-04-25 17:47:45 +0900

    [feat] record crud

---
+++ devway/src/main/java/com/ssafy/devway/domain/record/dto/response/CalendarRecordResDto.java
@@ -0,0 +1,19 @@
+package com.ssafy.devway.domain.record.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class CalendarRecordResDto {
+
+    private Long recordId;
+
+    private Boolean recordSuccess;
+
+    private LocalDate recordDate;
+}
