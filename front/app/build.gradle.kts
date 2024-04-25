4a6235a2b517b830d7e9529575760e8d6c84edbc
양성주 <ztjdwnz@naver.com>
2024-04-25 12:40:00 +0900

    [feat] splash activity

--- front/app/build.gradle.kts
+++ front/app/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
 }
