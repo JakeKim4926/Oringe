d87a3b220c17c83bfcee6d1b70ad5d29752f6eb2
양성주 <ztjdwnz@naver.com>
2024-04-28 18:30:08 +0900

    [feat] signup, signin

--- front/app/src/main/java/com/ssafy/oringe/activity/common/MainActivity.java
+++ front/app/src/main/java/com/ssafy/oringe/activity/common/MainActivity.java
@@ -1,6 +1,11 @@
 package com.ssafy.oringe.activity.common;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +13,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ssafy.oringe.R;
+import com.ssafy.oringe.activity.challenge.ChallengeListActivity;
+import com.ssafy.oringe.activity.login.SignupActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_record;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +36,26 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //오늘의 남은 오린지
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd", Locale.getDefault());
+        String currentDate = dateFormat.format(new Date());
+
+        TextView dateTextView = findViewById(R.id.text_today_oringe);
+
+        dateTextView.setText(currentDate + " \n 1개의 오린지가 남았어요");
+        dateTextView.setTypeface(null, Typeface.BOLD);
+
+        //챌린지리스트로 가기
+        FloatingActionButton btn_list;
+        btn_list = findViewById(R.id.btn_list);
+        btn_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ChallengeListActivity.class));
+
+            }
         });
     }
 }
