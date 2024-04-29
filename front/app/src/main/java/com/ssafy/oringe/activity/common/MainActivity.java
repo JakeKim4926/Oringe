37bdc509f20221006f288c4e131e3339783e1c50
양성주 <ztjdwnz@naver.com>
2024-04-29 09:56:52 +0900

    [fix] manifest

--- front/app/src/main/java/com/ssafy/oringe/activity/common/MainActivity.java
+++ front/app/src/main/java/com/ssafy/oringe/activity/common/MainActivity.java
@@ -49,6 +49,12 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+
         //메인 인사
 //        TextView mainHi = findViewById(R.id.text_nickname_hi);
 
