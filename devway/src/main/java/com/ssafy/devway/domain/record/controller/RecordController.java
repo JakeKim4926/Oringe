075ca43de054f04d5a2193c42107716b4973f718
김준섭 <wns1915@naver.com>
2024-05-03 15:17:02 +0900

    [feat] add setting success on record

--- devway/src/main/java/com/ssafy/devway/domain/record/controller/RecordController.java
+++ devway/src/main/java/com/ssafy/devway/domain/record/controller/RecordController.java
@@ -20,30 +20,30 @@ import java.util.List;
 @Tag(name = "인증", description = "Record API")
 public class RecordController {
 
-    private final RecordService recordService;
+  private final RecordService recordService;
 
-    @PostMapping("/record")
-    @Operation(summary = "인증 생성")
-    public ResponseEntity<Record> postRecord(@RequestBody RecordCreateReqDto dto, Long memberId) {
-        return ResponseEntity.ok(recordService.insertRecord(dto, memberId));
-    }
+  @PostMapping("/record")
+  @Operation(summary = "인증 생성")
+  public ResponseEntity<Record> postRecord(@RequestBody RecordCreateReqDto dto, Long memberId) {
+    return ResponseEntity.ok(recordService.insertRecord(dto, memberId));
+  }
 
-    @GetMapping("/record/challenge/{challengeId}")
-    @Operation(summary = "특정 챌린지의 월 별 인증 전체 조회")
-    public ResponseEntity<List<CalendarRecordResDto>> getCalendarRecord(Long challengeId,
-        Long memberId, int month) {
-        return ResponseEntity.ok(recordService.selectCalendarRecord(challengeId, memberId, month));
-    }
+  @GetMapping("/record/challenge/{challengeId}")
+  @Operation(summary = "특정 챌린지의 월 별 인증 전체 조회")
+  public ResponseEntity<List<CalendarRecordResDto>> getCalendarRecord(Long challengeId,
+      Long memberId, int month) {
+    return ResponseEntity.ok(recordService.selectCalendarRecord(challengeId, memberId, month));
+  }
 
-    @GetMapping("/record/{recordId}")
-    @Operation(summary = "인증 상세 조회")
-    public ResponseEntity<Record> getRecord(@PathVariable Long recordId) {
-        return ResponseEntity.ok(recordService.selectRecord(recordId));
-    }
+  @GetMapping("/record/{recordId}")
+  @Operation(summary = "인증 상세 조회")
+  public ResponseEntity<Record> getRecord(@PathVariable Long recordId) {
+    return ResponseEntity.ok(recordService.selectRecord(recordId));
+  }
 
-    @DeleteMapping("/record/{recordId}")
-    @Operation(summary = "인증 삭제")
-    public ResponseEntity<Long> deleteRecord(@PathVariable Long recordId) {
-        return ResponseEntity.ok(recordService.deleteRecord(recordId));
-    }
+  @PatchMapping("/record/success/{recordId}")
+  @Operation(summary = "인증 성공 하기")
+  public ResponseEntity<Record> setSuccess(@PathVariable Long recordId) {
+    return recordService.setSuccess(recordId);
+  }
 }
--- devway/src/main/java/com/ssafy/devway/domain/record/service/RecordService.java
+++ devway/src/main/java/com/ssafy/devway/domain/record/service/RecordService.java
@@ -11,6 +11,7 @@ import com.ssafy.devway.domain.record.repository.RecordRespository;
 import com.ssafy.devway.global.config.autoIncrementSequence.service.AutoIncrementSequenceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,69 +25,79 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class RecordService {
 
-    private final RecordRespository recordRespository;
-    private final MemberRepository memberRepository;
-    private final ChallengeRepository challengeRepository;
-    private final AutoIncrementSequenceService autoIncrementSequenceService;
+  private final RecordRespository recordRespository;
+  private final MemberRepository memberRepository;
+  private final ChallengeRepository challengeRepository;
+  private final AutoIncrementSequenceService autoIncrementSequenceService;
 
-    /*
-     * 4.1 인증 생성
-     * */
-    public Record insertRecord(RecordCreateReqDto dto, Long memberId) {
-        Member member = memberRepository.findByMemberId(memberId);
-        Challenge challenge = challengeRepository.findByChallengeId(dto.getChallengeId());
-        LocalDate today = LocalDate.now();
-        Record record = Record.builder()
-            .recordId(autoIncrementSequenceService.generateSequence(Record.SEQUENCE_NAME))
-            .challenge(challenge)
-            .member(member)
-            .recordDate(today)
-            .recordSuccess(true)
-            .recordTemplates(dto.getRecordTemplates())
-            .build();
+  /*
+   * 4.1 인증 생성
+   * */
+  public Record insertRecord(RecordCreateReqDto dto, Long memberId) {
+    Member member = memberRepository.findByMemberId(memberId);
+    Challenge challenge = challengeRepository.findByChallengeId(dto.getChallengeId());
+    LocalDate today = LocalDate.now();
+    Record record = Record.builder()
+        .recordId(autoIncrementSequenceService.generateSequence(Record.SEQUENCE_NAME))
+        .challenge(challenge)
+        .member(member)
+        .recordDate(today)
+        .recordSuccess(true)
+        .recordTemplates(dto.getRecordTemplates())
+        .build();
 
-        recordRespository.save(record);
+    recordRespository.save(record);
 
-        return record;
-    }
+    return record;
+  }
 
-    /*
-     * 4.2 특정 챌린지의 월 별 인증 전체 조회
-     * */
-    @Transactional(readOnly = true)
-    public List<CalendarRecordResDto> selectCalendarRecord(Long challengeId, Long memberId,
-        int month) {
-        List<Record> recordList = recordRespository.findByChallenge_ChallengeIdAndMember_MemberIdOrderByRecordDateAsc(
-            challengeId, memberId);
-        System.out.println(recordList);
-        return recordList.stream()
-            .filter(record -> record.getRecordDate().getMonthValue() == month)
-            .map(this::convertToCalendarRecordResDto)
-            .collect(Collectors.toList());
-    }
+  /*
+   * 4.2 특정 챌린지의 월 별 인증 전체 조회
+   * */
+  @Transactional(readOnly = true)
+  public List<CalendarRecordResDto> selectCalendarRecord(Long challengeId, Long memberId,
+      int month) {
+    List<Record> recordList = recordRespository.findByChallenge_ChallengeIdAndMember_MemberIdOrderByRecordDateAsc(
+        challengeId, memberId);
+    System.out.println(recordList);
+    return recordList.stream()
+        .filter(record -> record.getRecordDate().getMonthValue() == month)
+        .map(this::convertToCalendarRecordResDto)
+        .collect(Collectors.toList());
+  }
 
-    /*
-     * 4.3 특정 인증 상세 조회
-     * */
-    @Transactional(readOnly = true)
-    public Record selectRecord(Long recordId) {
-        return recordRespository.findByRecordId(recordId);
-    }
+  /*
+   * 4.3 특정 인증 상세 조회
+   * */
+  @Transactional(readOnly = true)
+  public Record selectRecord(Long recordId) {
+    return recordRespository.findByRecordId(recordId);
+  }
 
-    /*
-     * 4.4 특정 인증 삭제
-     * */
-    public Long deleteRecord(Long recordId) {
-        recordRespository.deleteById(recordId);
-        return recordId;
-    }
+  /*
+   * 4.4 특정 인증 삭제
+   * */
+  public Long deleteRecord(Long recordId) {
+    recordRespository.deleteById(recordId);
+    return recordId;
+  }
 
-    private CalendarRecordResDto convertToCalendarRecordResDto(Record record) {
-        return new CalendarRecordResDto(
-            record.getRecordId(),
-            record.getRecordSuccess(),
-            record.getRecordDate()
-        );
-    }
+  public ResponseEntity<Record> setSuccess(Long recordId) {
+    Record byRecordId = recordRespository.findByRecordId(recordId);
+      if (byRecordId == null) {
+          return ResponseEntity.notFound().build();
+      }
+
+    byRecordId.setRecordSuccess(true);
+    return ResponseEntity.ok(byRecordId);
+  }
+
+  private CalendarRecordResDto convertToCalendarRecordResDto(Record record) {
+    return new CalendarRecordResDto(
+        record.getRecordId(),
+        record.getRecordSuccess(),
+        record.getRecordDate()
+    );
+  }
 
 }
