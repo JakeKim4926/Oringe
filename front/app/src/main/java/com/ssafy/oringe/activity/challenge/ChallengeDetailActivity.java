75d3848d26157ad21e1f4ac2fabdfd5c4a1e8a25
김동학 <kdh19583@gmail.com>
2024-05-06 18:10:48 +0900

    [feat] create ChallengeDetail elements

---
+++ front/app/src/main/java/com/ssafy/oringe/activity/challenge/ChallengeDetailActivity.java
@@ -0,0 +1,13 @@
+package com.ssafy.oringe.activity.challenge;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.ssafy.oringe.R;
+
+public class ChallengeDetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge_detail);
+    }
+}
