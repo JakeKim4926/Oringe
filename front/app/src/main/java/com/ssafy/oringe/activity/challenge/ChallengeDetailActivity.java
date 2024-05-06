5186fa66bc2346effbad95430cef255e56bf0009
김동학 <kdh19583@gmail.com>
2024-05-06 18:22:51 +0900

    [feat] list에서 detail로 화면 이동 코드만 추가

--- front/app/src/main/java/com/ssafy/oringe/activity/challenge/ChallengeDetailActivity.java
+++ front/app/src/main/java/com/ssafy/oringe/activity/challenge/ChallengeDetailActivity.java
@@ -10,4 +10,13 @@ public class ChallengeDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge_detail);
     }
+
 }
+// ChallengeListActivity에 추가할것.
+//    challengeView.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            Intent intent = new Intent(ChallengeListActivity.this, ChallengeDetailActivity.class);
+//            startActivity(intent);
+//        }
+//    });
