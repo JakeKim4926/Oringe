630be8d46dcca0bad1e21408b86beda2bf9775f3
김준섭 <wns1915@naver.com>
2024-05-17 16:40:25 +0900

    [chore] fix typo

--- front/app/src/main/java/com/ssafy/oringe/activity/record/RecordCreateActivity.java
+++ front/app/src/main/java/com/ssafy/oringe/activity/record/RecordCreateActivity.java
@@ -394,7 +394,7 @@ public class RecordCreateActivity extends AppCompatActivity implements AdapterVi
                         return;
                     }
 
-                    Pattern pattern = Pattern.compile("^[a-zA-Z\\s]+$");
+                    Pattern pattern = Pattern.compile("^[a-zA-Z\\s0-9]+$");
                     if (!pattern.matcher(inputText).matches()) {
                         Toast.makeText(RecordCreateActivity.this, "영어만 입력 가능합니다", Toast.LENGTH_SHORT).show();
                         return;
