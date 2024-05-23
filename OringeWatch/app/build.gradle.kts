cb0c601b94a90f35731355518c3faeb3904128a2
김준섭 <wns1915@naver.com>
2024-05-23 09:32:10 +0900

    [feat] add dependencies

--- OringeWatch/app/build.gradle.kts
+++ OringeWatch/app/build.gradle.kts
@@ -62,4 +62,34 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.firebaseui:firebase-ui-auth:8.0.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.google.android.gms:play-services-auth:20.2.0")
+
+    // OkHttp
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // Retrofit Gson Converter
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+    implementation("com.arthenica:mobile-ffmpeg-full:4.4.LTS")
+
+    implementation("org.projectlombok:lombok:1.18.20")
+    annotationProcessor("org.projectlombok:lombok:1.18.20")
+
+    //for desugaring
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+
+    // The view calendar library
+    implementation ("com.kizitonwose.calendar:view:2.5.1")
+
+    // The compose calendar library
+    implementation ("com.kizitonwose.calendar:compose:2.5.1")
 }
