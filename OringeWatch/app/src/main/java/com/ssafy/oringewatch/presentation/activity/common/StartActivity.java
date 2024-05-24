9e50426aa48bfd18f0e1e49a271098e05fc1c4bb
김준섭 <wns1915@naver.com>
2024-05-24 20:21:59 +0900

    [fix] fix starting error

--- OringeWatch/app/src/main/java/com/ssafy/oringewatch/presentation/activity/common/StartActivity.java
+++ OringeWatch/app/src/main/java/com/ssafy/oringewatch/presentation/activity/common/StartActivity.java
@@ -1,14 +1,19 @@
 package com.ssafy.oringewatch.presentation.activity.common;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.widget.ImageView;
 
 import androidx.activity.ComponentActivity;
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
 import com.ssafy.oringewatch.R;
 import com.ssafy.oringewatch.presentation.activity.MainActivity;
 
@@ -19,14 +24,34 @@ public class StartActivity extends ComponentActivity {
         setContentView(R.layout.activity_start);
 
         ImageView imageViewSplash = findViewById(R.id.imageViewSplash);
-        Glide.with(this).load(R.drawable.splash).into(imageViewSplash);
 
-        // 딜레이 후 MainActivity로 전환
-        int splashScreenDuration = 3000; // 이미지의 표시 시간 (밀리초)
-        new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            Intent intent = new Intent(StartActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }, splashScreenDuration);
+        Glide.with(this)
+                .load(R.drawable.splash)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        // 실패 시 동작 정의
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        if (resource instanceof GifDrawable) {
+                            GifDrawable gifDrawable = (GifDrawable) resource;
+                            gifDrawable.setLoopCount(1); // 한 사이클만 재생
+                            gifDrawable.registerAnimationCallback(new Animatable2Compat.AnimationCallback() {
+                                @Override
+                                public void onAnimationEnd(Drawable drawable) {
+                                    // GIF 애니메이션이 끝났을 때 MainActivity로 전환
+                                    Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            });
+                        }
+                        return false;
+                    }
+                })
+                .into(imageViewSplash);
     }
 }
