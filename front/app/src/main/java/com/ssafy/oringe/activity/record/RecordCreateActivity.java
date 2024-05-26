938f16d644272831fe2806b59766d048458ef128
김동학 <kdh19583@gmail.com>
2024-05-27 08:41:16 +0900

    [fix] modify regexp for type in TTS

--- front/app/src/main/java/com/ssafy/oringe/activity/record/RecordCreateActivity.java
+++ front/app/src/main/java/com/ssafy/oringe/activity/record/RecordCreateActivity.java
@@ -212,7 +212,7 @@ public class RecordCreateActivity extends AppCompatActivity implements AdapterVi
                         return;
                     }
 
-                    Pattern pattern = Pattern.compile("^[가-힣0-9,.'\"!?\\s]+$");
+                    Pattern pattern = Pattern.compile("^[가-힣ㄱ-ㅎㅏ-ㅣ0-9,.'\"!?\\s]+$");
                     if (!pattern.matcher(inputText).matches()) {
                         Toast.makeText(RecordCreateActivity.this, "한글과 숫자만 입력 가능합니다", Toast.LENGTH_SHORT).show();
                         return;
